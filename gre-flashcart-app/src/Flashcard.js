17b5417034f12f56441fd6e872a6900e8a976382
Saurabh Kishore <gks210199@gmail.com>
2022-05-17 21:27:36 -0400

    rough implementation of flashcard complete

--- gre-flashcart-app/src/Flashcard.js
+++ gre-flashcart-app/src/Flashcard.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Flashcard({ flashcard }) {
+  const [flip, setFlip] = useState(false)
+
+
   return (
-    <div>
-      {flashcard.word}
+    <div 
+      //change classs name depending on state of flash card
+      className={`card ${flip? 'flip': ''}`}
+      onClick={() => setFlip(!flip)}
+    >
+      <div className='front'>{flashcard.word}</div>
+      <div className='back'>{flashcard.definition}</div>
     </div>
   )
 }
--- gre-flashcart-app/src/FlashcardList.js
+++ gre-flashcart-app/src/FlashcardList.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Flashcard from './Flashcard'
 
-export default function FlashcardList({flashcards}) {
+export default function FlashcardList({ flashcards }) {
   return (
     <div className='card-grid'>
         {flashcards.map(flashcard=> {
